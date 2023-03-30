b646fb66d0b666556e0aaf374bc363f1f41d657e
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-03-29 21:08:26 -0300

    sent email endpoint fixed

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ app.use(express.static(__dirname + '/public/'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/sent', (req, res) => {
+    res.sendFile(__dirname + '/sentEmail.html')
+})
