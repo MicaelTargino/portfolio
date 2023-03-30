77cf93cebfe47600489c2f471787587c44ecdd0f
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-03-29 21:00:51 -0300

    finish repository

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('server up and running on port 3000')
+})
+
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
