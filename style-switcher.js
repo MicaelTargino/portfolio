644debd6c25a70171af344b6c20e87269462d563
MicaelTargino <micaelgomestargino@gmail.com>
2024-09-05 19:42:10 -0300

    v2

--- style-switcher.js
+++ style-switcher.js
@@ -1,3 +1,26 @@
+const svgTemplate2 = `
+<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M7 8L3 12L7 16" stroke="{color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M17 8L21 12L17 16" stroke="{color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M14 4L9.8589 19.4548" stroke="{color}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+
+function setFavicon() {
+    const skinColor = getComputedStyle(document.documentElement).getPropertyValue('--skin-color').trim();
+    const svgContent = svgTemplate2.replace(/{color}/g, skinColor);
+    const blob = new Blob([svgContent], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = url;
+    link.type = 'image/svg+xml';
+
+    const existingIcons = document.querySelectorAll('link[rel="icon"]');
+    existingIcons.forEach(icon => icon.parentNode.removeChild(icon));
+
+    document.head.appendChild(link);
+}
+
 // toggle style switcher 
 const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
 styleSwitcherToggle.addEventListener('click', () => {
@@ -14,13 +37,18 @@ window.addEventListener('scroll', () => {
 // theme colors 
 const alternateStyles = document.querySelectorAll('.alternate-style');
 function setActiveStyle(color) {
-    alternateStyles.forEach((style) => {
-        if (color === style.getAttribute('title')) {
-            style.removeAttribute('disabled');
-        } else {
-            style.setAttribute('disabled', 'true');
-        }
-    })
+    var r = document.querySelector(':root');
+    r.style.setProperty('--skin-color', color);
+    // alternateStyles.forEach((style) => {
+    //     if (color === style.getAttribute('title')) {
+    //         style.removeAttribute('disabled');
+    //     } else {
+    //         style.setAttribute('disabled', 'true');
+    //     }
+    // })
+    setTimeout(() => {
+        setFavicon();
+    }, 500)
 }
 
 // day-night 
@@ -30,6 +58,7 @@ dayNight.addEventListener('click', () => {
     dayNight.querySelector('i').classList.toggle('fa-moon');
     document.body.classList.toggle('dark');
 })
+
 window.addEventListener('load', () => {
     if (document.body.classList.contains('dark')) {
         dayNight.querySelector('i').classList.add('fa-sun');
